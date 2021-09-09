290b97c04cf553cb678424bf269e2bb194a859d7
silvestr1 <ssilvers@inbox.lv>
2021-09-09 10:06:59 +0100

    first commit

---
+++ silvestrs.js
@@ -0,0 +1,158 @@
+alert("Paziņojuma saturs");
+
+function zimet(){
+    var pamatne = document.getElementById("ManaVieta");
+    var zime = pamatne.getContext("2d");
+//apakšējā bumba
+    zime.beginPath();
+    zime.arc(150, 150, 60, 0, 2 * Math.PI);
+    zime.stroke();
+    zime.fillStyle = "blue";
+    zime.fill();
+    zime.strokeStyle = "blue"
+    zime.stroke();
+//vidējā bumba
+    zime.beginPath();
+    zime.arc(150, 150, 30, 0, 2* Math.PI);
+    zime.stroke();
+    zime.fillStyle = "white";
+    zime.fill();
+    zime.strokeStyle = "white"
+    zime.stroke();
+
+    //apakšējā bumba
+    zime.beginPath();
+    zime.arc(250, 150, 60, 0, 2 * Math.PI);
+    zime.stroke();
+    zime.fillStyle = "red";
+    zime.fill();
+    zime.strokeStyle = "red"
+    zime.stroke();
+
+    //vidējā bumba
+    zime.beginPath();
+    zime.arc(250, 150, 30, 0, 2* Math.PI);
+    zime.stroke();
+    zime.fillStyle = "yellow";
+    zime.fill();
+    zime.strokeStyle = "yellow"
+    zime.stroke();
+
+ //apakšējā bumba
+ zime.beginPath();
+ zime.arc(50, 150, 60, 0, 2 * Math.PI);
+ zime.stroke();
+ zime.fillStyle = "pink";
+ zime.fill();
+ zime.strokeStyle = "pink"
+ zime.stroke();
+
+ //vidējā bumba
+ zime.beginPath();
+ zime.arc(50, 150, 30, 0, 2* Math.PI);
+ zime.stroke();
+ zime.fillStyle = "gray";
+ zime.fill();
+ zime.strokeStyle = "gray"
+ zime.stroke();
+
+ //apakšējā bumba
+ zime.beginPath();
+ zime.arc(50, 250, 60, 0, 2 * Math.PI);
+ zime.stroke();
+ zime.fillStyle = "black";
+ zime.fill();
+ zime.strokeStyle = "black"
+ zime.stroke();
+
+ //vidējā bumba
+ zime.beginPath();
+ zime.arc(50, 250, 30, 0, 2* Math.PI);
+ zime.stroke();
+ zime.fillStyle = "black";
+ zime.fill();
+ zime.strokeStyle = "black"
+ zime.stroke();
+
+ //apakšējā bumba
+ zime.beginPath();
+ zime.arc(150, 250, 60, 0, 2 * Math.PI);
+ zime.stroke();
+ zime.fillStyle = "white";
+ zime.fill();
+ zime.strokeStyle = "white"
+ zime.stroke();
+
+ //apakšējā bumba
+ zime.beginPath();
+ zime.arc(250, 250, 60, 0, 2 * Math.PI);
+ zime.stroke();
+ zime.fillStyle = "black";
+ zime.fill();
+ zime.strokeStyle = "black"
+ zime.stroke();
+
+ //vidējā bumba
+ zime.beginPath();
+ zime.arc(250, 250, 30, 0, 2* Math.PI);
+ zime.stroke();
+ zime.fillStyle = "black";
+ zime.fill();
+ zime.strokeStyle = "black"
+ zime.stroke();
+
+ //apakšējā bumba
+ zime.beginPath();
+ zime.arc(250, 50, 60, 0, 2 * Math.PI);
+ zime.stroke();
+ zime.fillStyle = "black";
+ zime.fill();
+ zime.strokeStyle = "black"
+ zime.stroke();
+
+ //vidējā bumba
+ zime.beginPath();
+ zime.arc(250, 50, 30, 0, 2* Math.PI);
+ zime.stroke();
+ zime.fillStyle = "black";
+ zime.fill();
+ zime.strokeStyle = "black"
+ zime.stroke();
+
+//apakšējā bumba
+zime.beginPath();
+zime.arc(50, 50, 60, 0, 2 * Math.PI);
+zime.stroke();
+zime.fillStyle = "black";
+zime.fill();
+zime.strokeStyle = "black"
+zime.stroke();
+
+//vidējā bumba
+zime.beginPath();
+zime.arc(50, 50, 30, 0, 2* Math.PI);
+zime.stroke();
+zime.fillStyle = "black";
+zime.fill();
+zime.strokeStyle = "black"
+zime.stroke();
+
+//apakšējā bumba
+zime.beginPath();
+zime.arc(150, 50, 60, 0, 2 * Math.PI);
+zime.stroke();
+zime.fillStyle = "white";
+zime.fill();
+zime.strokeStyle = "white"
+zime.stroke();
+
+//vidējā bumba
+zime.beginPath();
+zime.arc(150, 50, 30, 0, 2* Math.PI);
+zime.stroke();
+zime.fillStyle = "white";
+zime.fill();
+zime.strokeStyle = "white"
+zime.stroke();
+
+}
